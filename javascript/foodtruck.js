cdf324c947fd2f84ac099e4c3bf74bc01891df68
Waseem A. Abd Elhadi <wasimabdelhadi78@gmail.com>
2022-11-17 21:09:42 +0200

    Modifie files

--- javascript/foodtruck.js
+++ javascript/foodtruck.js
@@ -1,10 +1,22 @@
 let myButton = document.getElementById("group");
+let myHeader = document.getElementById("header");
+let myListHeader = document.getElementById("list");
+let cssAfter = window.getComputedStyle(myListHeader, '::after');
+
 
 window.onscroll = function () {
     if (window.pageYOffset >= 500) {
         myButton.style.display = "block";
     } else {
         myButton.style.display = "none";
+    }
+    if (window.pageYOffset >= 200) {
+        myHeader.style.backgroundColor = "rgb(245 158 11 / 88%)";
+        myHeader.style.padding = "10px 0";
+        myListHeader.style.setProperty('--afterBack', 'white');
+    } else {
+        myHeader.style.background ="none";
+        myListHeader.style.setProperty('--afterBack', '#f59e0b');
     }
 };
 
